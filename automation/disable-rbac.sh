32c35e6c72eac35f05317d5f652f8725d65ea36f
chadkong <chad.ingle@konghq.com>
2022-05-25 07:16:06 -0600

    modifying scripts and adding docker

---
+++ automation/disable-rbac.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Disable RBAC
+cat << EOF > admin_gui_session_conf
+{
+    "cookie_name":"admin_session",
+    "cookie_samesite":"off",
+    "secret":"kong",
+    "cookie_secure":false,
+    "storage":"kong"
+}
+EOF
+kubectl create secret generic kong-session-config -n kong \
+--save-config \
+--dry-run=client \
+--from-file=admin_gui_session_conf \
+-o yaml | \
+kubectl apply -f -
+
+helm upgrade -f ./helm/cp-values.yaml kong kong/kong -n kong \
+--set manager.ingress.hostname=$KONG_MANAGER_URI \
+--set portal.ingress.hostname=$KONG_PORTAL_GUI_HOST \
+--set admin.ingress.hostname=$KONG_ADMIN_API_URI \
+--set portalapi.ingress.hostname=$KONG_PORTAL_API_URI
+
+watch "kubectl get pods -n kong"
