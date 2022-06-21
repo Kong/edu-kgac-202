0fbd997284688920b98b45b929e5cf7877add444
chadkong <chad.ingle@konghq.com>
2022-06-21 16:02:14 -0600

    combining cp and dp

--- automation/install.sh
+++ automation/install.sh
@@ -172,22 +172,22 @@ WAIT_POD=`kubectl get pods --selector=app=kong-kong -n kong -o jsonpath='{.items
 echo "Kong control plane pod exists and now waiting for it to come online..."
 kubectl wait --for=condition=Ready --timeout=300s pod $WAIT_POD -n kong
 
-# Deploy Kong Data Plane
-kubectl create namespace kong-dp
-#kubectl create secret generic kong-enterprise-superuser-password --from-literal=password=password -n kong-dp
-kubectl create secret tls kong-cluster-cert --cert=./cluster.crt --key=./cluster.key -n kong-dp
-kubectl create secret generic kong-enterprise-license -n kong-dp --from-file=license=/etc/kong/license.json
-helm install -f ./helm/dp-values.yaml kong-dp kong/kong -n kong-dp \
---set proxy.ingress.hostname=${KONG_PROXY_URI}
+# # Deploy Kong Data Plane
+# kubectl create namespace kong-dp
+# #kubectl create secret generic kong-enterprise-superuser-password --from-literal=password=password -n kong-dp
+# kubectl create secret tls kong-cluster-cert --cert=./cluster.crt --key=./cluster.key -n kong-dp
+# kubectl create secret generic kong-enterprise-license -n kong-dp --from-file=license=/etc/kong/license.json
+# helm install -f ./helm/dp-values.yaml kong-dp kong/kong -n kong-dp \
+# --set proxy.ingress.hostname=${KONG_PROXY_URI}
 
-# Wait for Kong DP Pods
-while [[ -z $(kubectl get pods --selector=app=kong-dp-kong -n kong-dp -o jsonpath='{.items[*].metadata.name}' 2>/dev/null) ]]; do
-  echo "Waiting for kong data plane pod to exist..."
-  sleep 1
-done
-WAIT_POD=`kubectl get pods --selector=app=kong-dp-kong -n kong-dp -o jsonpath='{.items[*].metadata.name}'`
-echo "Kong data plane pod exists and now waiting for it to come online..."
-kubectl wait --for=condition=Ready --timeout=300s pod $WAIT_POD -n kong-dp
+# # Wait for Kong DP Pods
+# while [[ -z $(kubectl get pods --selector=app=kong-dp-kong -n kong-dp -o jsonpath='{.items[*].metadata.name}' 2>/dev/null) ]]; do
+#   echo "Waiting for kong data plane pod to exist..."
+#   sleep 1
+# done
+# WAIT_POD=`kubectl get pods --selector=app=kong-dp-kong -n kong-dp -o jsonpath='{.items[*].metadata.name}'`
+# echo "Kong data plane pod exists and now waiting for it to come online..."
+# kubectl wait --for=condition=Ready --timeout=300s pod $WAIT_POD -n kong-dp
 
 # Deploy some course components
 kubectl apply -f apps/httpbin.yaml
--- automation/patch.sh
+++ automation/patch.sh
@@ -5,8 +5,8 @@ sed -i 's/tag: "2.2"/tag: "2.4"/g' ./helm/cp-values.yaml
 sed -i 's/tag: "2.7-alpine"/tag: "2.8.1.1-alpine"/g' ./helm/dp-values.yaml 
 sed -i 's/tag: "2.7-alpine"/tag: "2.8.1.1-alpine"/g' ./helm/cp-values.yaml 
 
-helm upgrade -f ./helm/dp-values.yaml kong-dp kong/kong -n kong-dp \
---set proxy.ingress.hostname=$KONG_PROXY_URI
+# helm upgrade -f ./helm/dp-values.yaml kong-dp kong/kong -n kong-dp \
+# --set proxy.ingress.hostname=$KONG_PROXY_URI
 
 helm upgrade -f ./helm/cp-values.yaml kong kong/kong -n kong \
 --set manager.ingress.hostname=$KONG_MANAGER_URI \
