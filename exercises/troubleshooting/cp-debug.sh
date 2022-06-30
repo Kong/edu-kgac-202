12d65249a3212efb528706935caac0a5840783fc
chadkong <chad.ingle@konghq.com>
2022-06-30 15:36:02 -0600

    debug deploy

---
+++ exercises/troubleshooting/cp-debug.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+CURRENTDIR=`pwd`
+cd /home/labuser/kong-course-gateway-ops-for-kubernetes/exercises/troubleshooting
+
+sed -i "s/admin_gui_url:.*/admin_gui_url: https:\/\/$KONG_MANAGER_URI/g" ./cp-values-debug.yaml
+sed -i "s/admin_api_url:.*/admin_api_url: https:\/\/$KONG_ADMIN_API_URI/g" ./cp-values-debug.yaml
+sed -i "s/admin_api_uri:.*/admin_api_uri: $KONG_ADMIN_API_URI/g" ./cp-values-debug.yaml
+sed -i "s/proxy_url:.*/proxy_url: https:\/\/$KONG_PROXY_URI/g" ./cp-values-debug.yaml
+sed -i "s/portal_api_url:.*/portal_api_url: https:\/\/$KONG_PORTAL_API_URI/g" ./cp-values-debug.yaml
+sed -i "s/portal_gui_host:.*/portal_gui_host: $KONG_PORTAL_GUI_HOST/g" ./cp-values-debug.yaml
+
+helm upgrade -f ./cp-values-debug.yaml kong kong/kong -n kong \
+--set manager.ingress.hostname=$KONG_MANAGER_URI \
+--set portal.ingress.hostname=$KONG_PORTAL_GUI_HOST \
+--set admin.ingress.hostname=$KONG_ADMIN_API_URI \
+--set portalapi.ingress.hostname=$KONG_PORTAL_API_URI 
+
+cd $CURRENTDIR
