ab81d7d23f32e904b468339851ae367a32736a55
chadkong <chad.ingle@konghq.com>
2022-07-19 14:06:39 -0600

    adding wait

--- base/teardown-cluster.sh
+++ base/teardown-cluster.sh
@@ -2,10 +2,10 @@
 export KUBECONFIG=/home/labuser/.kube/config
 
 # Remove DP
-helm uninstall kong-dp -n kong-dp
+helm uninstall kong-dp -n kong-dp --wait
 
 # Remove CP
-helm uninstall kong -n kong
+helm uninstall kong -n kong --wait
 
 # Remove Namespaces
 kubectl delete ns kong
