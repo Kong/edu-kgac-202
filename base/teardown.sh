08d205cd48244688ac05b6265fa76ccaf17a3fd1
chadkong <chad.ingle@konghq.com>
2022-07-21 08:11:09 -0600

    making teardown more efficient

--- base/teardown.sh
+++ base/teardown.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 export KUBECONFIG=/home/labuser/.kube/config
 
-# Remove DP
-helm uninstall kong-dp -n kong-dp --wait
-
-# Remove CP
-helm uninstall kong -n kong --wait
-
-# Remove Namespaces
-kubectl delete ns kong
-kubectl delete ns kong-dp
-
 # Delete kind cluster
 kind delete cluster --name avl
 
