5a89c27dbe6379c13c3474a7a9056a938934190e
chadkong <chad.ingle@konghq.com>
2022-07-25 13:26:41 -0600

    adv plugins

--- base/install.sh
+++ base/install.sh
@@ -65,11 +65,14 @@ kubectl apply -f https://projectcalico.docs.tigera.io/manifests/calico.yaml
 kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
 
 # K8s resources, prometheus, grafana, and statsd
-kubectl create ns monitoring
+kubectl create namespace monitoring
+kubectl create namespace kong
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm install -f /home/labuser/kong-course-gateway-ops-for-kubernetes/exercises/monitoring/prometheus-values.yaml prometheus prometheus-community/kube-prometheus-stack -n monitoring --wait
 helm install -f /home/labuser/kong-course-gateway-ops-for-kubernetes/exercises/monitoring/statsd-values.yaml statsd prometheus-community/prometheus-statsd-exporter -n monitoring --wait
+helm install redis bitnami/redis -n kong --set auth.enabled=false
 
 # Create Keys and Certs, Namespace, and Load into K8s
 cd /home/labuser/kong-course-gateway-ops-for-kubernetes
@@ -77,7 +80,6 @@ openssl rand -writerand .rnd
 openssl req -new -x509 -nodes -newkey ec:<(openssl ecparam -name secp384r1) \
   -keyout ./cluster.key -out ./cluster.crt \
   -days 1095 -subj "/CN=kong_clustering"
-kubectl create namespace kong
 kubectl create secret tls kong-cluster-cert --cert=./cluster.crt --key=./cluster.key -n kong
 
 # Load License
