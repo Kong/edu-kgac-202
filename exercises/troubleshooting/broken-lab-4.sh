8e9bba33dc27f6abd1013d91fb8d7ee2c0b25a18
chadkong <chad.ingle@konghq.com>
2022-07-28 13:41:10 -0600

    lab 4 mods

--- exercises/troubleshooting/broken-lab-4.sh
+++ exercises/troubleshooting/broken-lab-4.sh
@@ -21,25 +21,11 @@ helm upgrade -f ./exercises/troubleshooting/cp-broken-values.yaml kong kong/kong
 --set admin.ingress.hostname=$KONG_ADMIN_API_URI \
 --set portalapi.ingress.hostname=$KONG_PORTAL_API_URI 
 
-# Deploy Kong Data Plane
-kubectl create namespace kong-data-plane
-kubectl create secret tls kong-cluster-cert --cert=./cluster.crt --key=./cluster.key -n kong-data-plane
-kubectl create secret generic kong-enterprise-license -n kong-data-plane --from-file=license=/etc/kong/license.json
-helm install -f ./exercises/troubleshooting/dp-broken-values.yaml kong-data-plane kong/kong -n kong-data-plane \
---set proxy.ingress.hostname=${KONG_PROXY_URI}
-
-# Wait for Kong DP Pods
-while [[ -z $(kubectl get pods --selector=app=kong-data-plane-kong -n kong-data-plane -o jsonpath='{.items[*].metadata.name}' 2>/dev/null) ]]; do
-  echo "Waiting for kong data plane pod to exist..."
-  sleep 1
-done
-WAIT_POD=`kubectl get pods --selector=app=kong-data-plane-kong -n kong-data-plane -o jsonpath='{.items[*].metadata.name}'`
-echo "Kong data plane pod exists and now waiting for it to come online..."
-kubectl wait --for=condition=Ready --timeout=300s pod $WAIT_POD -n kong-data-plane
+helm upgrade -f ./exercises/troubleshooting/dp-broken-values.yaml kong-dp kong/kong -n kong-dp \
+--set proxy.ingress.hostname=$KONG_PROXY_URI
 
 # Deploy some configs
 kubectl apply -f ./exercises/troubleshooting/broken-lab-4.yaml
-source ./exercises/jwt/jane-jwt.sh
 
 # Change back to source directory
 cd $CURRENTDIR
