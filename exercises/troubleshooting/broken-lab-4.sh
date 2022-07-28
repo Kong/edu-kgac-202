b8be51b7d42f10c93e73deac9c138c3f58116b8e
chadkong <chad.ingle@konghq.com>
2022-07-28 14:14:49 -0600

    lab 4 final

--- exercises/troubleshooting/broken-lab-4.sh
+++ exercises/troubleshooting/broken-lab-4.sh
@@ -21,9 +21,20 @@ helm upgrade -f ./exercises/troubleshooting/cp-broken-values.yaml kong kong/kong
 --set admin.ingress.hostname=$KONG_ADMIN_API_URI \
 --set portalapi.ingress.hostname=$KONG_PORTAL_API_URI 
 
+# Deploy Kong Data Plane
 kubectl create namespace kong-dp
+kubectl create secret tls kong-cluster-cert --cert=./cluster.crt --key=./cluster.key -n kong-dp
+kubectl create secret generic kong-enterprise-license -n kong-dp --from-file=license=/etc/kong/license.json
 helm install -f ./exercises/troubleshooting/dp-broken-values.yaml kong-dp kong/kong -n kong-dp \
 --set proxy.ingress.hostname=$KONG_PROXY_URI
+
+# Wait for Kong DP Pods
+while [[ -z $(kubectl get pods --selector=app=kong-dp-kong -n kong-dp -o jsonpath='{.items[*].metadata.name}' 2>/dev/null) ]]; do
+  echo "Waiting for kong data plane pod to exist..."
+  sleep 1
+done
+WAIT_POD=`kubectl get pods --selector=app=kong-dp-kong -n kong-dp -o jsonpath='{.items[*].metadata.name}'`
+echo "Kong data plane pod exists and now waiting for it to come online..."
 
 # Deploy some configs
 kubectl apply -f ./exercises/troubleshooting/broken-lab-4.yaml
