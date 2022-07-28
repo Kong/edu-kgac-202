7f0897557e45732ea9bd4410e60b57a1d5b3cf0c
chadkong <chad.ingle@konghq.com>
2022-07-28 10:59:32 -0600

    broken lab 4

--- exercises/troubleshooting/broken-lab-4.sh
+++ exercises/troubleshooting/broken-lab-4.sh
@@ -5,7 +5,7 @@ CURRENTDIR=`pwd`
 cd /home/labuser/kong-course-gateway-ops-for-kubernetes/
 
 # Delete httpbin-demo namespace
-kubectl delete namespace httpbin-demo
+kubectl delete namespace httpbin-demo kong-dp
 
 # Upgrade Kong CP with Broken Values
 sed -i "s/admin_gui_url:.*/admin_gui_url: https:\/\/$KONG_MANAGER_URI/g" ./exercises/troubleshooting/cp-broken-values.yaml
@@ -21,8 +21,21 @@ helm upgrade -f ./exercises/troubleshooting/cp-broken-values.yaml kong kong/kong
 --set admin.ingress.hostname=$KONG_ADMIN_API_URI \
 --set portalapi.ingress.hostname=$KONG_PORTAL_API_URI 
 
-helm upgrade -f ./exercises/troubleshooting/dp-broken-values.yaml kong-dp kong/kong -n kong-dp \
---set proxy.ingress.hostname=$KONG_PROXY_URI
+# Deploy Kong Data Plane
+kubectl create namespace kong-data-plane
+kubectl create secret tls kong-cluster-cert --cert=./cluster.crt --key=./cluster.key -n kong-data-plane
+kubectl create secret generic kong-enterprise-license -n kong-data-plane --from-file=license=/etc/kong/license.json
+helm install -f ./exercises/troubleshooting/dp-broken-values.yaml kong-data-plane kong/kong -n kong-data-plane \
+--set proxy.ingress.hostname=${KONG_PROXY_URI}
+
+# Wait for Kong DP Pods
+while [[ -z $(kubectl get pods --selector=app=kong-data-plane-kong -n kong-data-plane -o jsonpath='{.items[*].metadata.name}' 2>/dev/null) ]]; do
+  echo "Waiting for kong data plane pod to exist..."
+  sleep 1
+done
+WAIT_POD=`kubectl get pods --selector=app=kong-data-plane-kong -n kong-data-plane -o jsonpath='{.items[*].metadata.name}'`
+echo "Kong data plane pod exists and now waiting for it to come online..."
+kubectl wait --for=condition=Ready --timeout=300s pod $WAIT_POD -n kong-data-plane
 
 # Deploy some configs
 kubectl apply -f ./exercises/troubleshooting/broken-lab-4.yaml
