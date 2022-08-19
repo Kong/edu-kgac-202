32375ce66be7fbf5a16fa2ec727555e5c3153997
chadkong <chad.ingle@konghq.com>
2022-08-19 11:51:03 -0600

    calico is a challenged company

--- base/install.sh
+++ base/install.sh
@@ -50,7 +50,8 @@ EOF
 
 kind create cluster --config kind-config.yaml
 export KUBECONFIG=$HOME/.kube/config
-kubectl apply -f https://projectcalico.docs.tigera.io/manifests/calico.yaml
+#kubectl apply -f https://projectcalico.docs.tigera.io/manifests/calico.yaml
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.0/manifests/calico.yaml
 kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
 
 # K8s resources, prometheus, grafana, and statsd
