6dae01613af381022e235679f5c080c66a6a8b67
johnfitzpatrick <jhnftzptrck@gmail.com>
2023-03-01 11:28:09 +0000

    dev

--- set-env.sh
+++ set-env.sh
@@ -6,5 +6,5 @@ export KONG_ADMIN_API_URI=http://$STRIGO_RESOURCE_DNS:30001
 export KONG_ADMIN_GUI_URL=http://$STRIGO_RESOURCE_DNs:30002
 export KONG_PORTAL_GUI_HOST=$STRIGO_RESOURCE_DNS:30003
 export KONG_PORTAL_API_URL=http://$STRIGO_RESOURCE_DNS:30004
-export KONG_LICENSE_DATA=$(cat /usr/local/kong/license.json)
+export KONG_LICENSE_DATA="$(cat /usr/local/kong/license.json)"
 
