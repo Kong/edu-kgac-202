a00ead9e1146603b01b4fc49dd1c77de581d3785
johnfitzpatrick <jhnftzptrck@gmail.com>
2023-03-02 14:41:01 +0000

    dev

--- exercises/jwt/create-jwt-token.sh
+++ exercises/jwt/create-jwt-token.sh
@@ -8,10 +8,9 @@
 # fi
 
 CONSUMER=jane
-echo "Creating JWT token for consumer '$CONSUMER'"
 
 HEADER=$(echo -n '{"alg":"RS256","typ":"JWT"}' | openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
-PAYLOAD=$(echo -n '{"iss":"'$CONSUMER'"}' | openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
+PAYLOAD=$(echo -n '{"iss":"'$CONSUMER-issuer'"}' | openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
 HEADER_PAYLOAD=$HEADER.$PAYLOAD
 PEM=$(cat ./$CONSUMER.pem)
 SIG=$(openssl dgst -sha256 -sign <(echo -n "${PEM}") <(echo -n "${HEADER_PAYLOAD}") | openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
