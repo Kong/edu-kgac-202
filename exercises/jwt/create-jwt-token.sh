d05684697d09c29e20ac695ea04bb93b9d083553
johnfitzpatrick <jhnftzptrck@gmail.com>
2023-03-02 14:23:12 +0000

    dev

---
+++ exercises/jwt/create-jwt-token.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# read -p "What is the consumer's name?" CONSUMER
+
+# req=$(http -h $KONG_ADMIN_API_URL/consumers/$CONSUMER | head -1 )
+# if [ "$req" != "HTTP/1.1 200 OK" ]; then
+#   echo User $CONSUMER does not exist
+# fi
+
+CONSUMER=jane
+echo "Creating JWT token for consumer '$CONSUMER'"
+
+HEADER=$(echo -n '{"alg":"RS256","typ":"JWT"}' | openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
+PAYLOAD=$(echo -n '{"iss":"'$CONSUMER'"}' | openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
+HEADER_PAYLOAD=$HEADER.$PAYLOAD
+PEM=$(cat ./$CONSUMER.pem)
+SIG=$(openssl dgst -sha256 -sign <(echo -n "${PEM}") <(echo -n "${HEADER_PAYLOAD}") | openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
+TOKEN=$HEADER.$PAYLOAD.$SIG
+$ echo $TOKEN
+
