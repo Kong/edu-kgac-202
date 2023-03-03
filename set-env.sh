11c35dd102371b1783f27763dbf3d8f8271f9e19
johnfitzpatrick <jhnftzptrck@gmail.com>
2023-03-03 19:11:19 +0000

    dev

--- set-env.sh
+++ set-env.sh
@@ -10,6 +10,7 @@ export KONG_PORTAL_API_URL=http://$STRIGO_RESOURCE_DNS:30004
 export KONGHOSTNAME="$STRIGO_RESOURCE_DNS"
 
 export KEYCLOAK_URI="http://$STRIGO_RESOURCE_DNS:8080/auth"
-export KEYCLOAK_REDIRECT_URI="http://$STRIGO_RESOURCE_DNS:8080/echo-request"
-export KEYCLOAK_CONFIG_ISSUER="http://keycloak:8080/auth/realms/kong/.well-known/openid-configuration"
-export CLIENT_SECRET="$(jq '.clients[] | select(.clientId == "kong")' /home/ubuntu//edu-kgac-202/exercises/oidc/kong_realm_template.json | jq .secret | xargs)"
+export KEYCLOAK_REDIRECT_URI="http://$STRIGO_RESOURCE_DNS:30000/oidc"
+export KEYCLOAK_CONFIG_ISSUER="http://$STRIGO_RESOURCE_DNS:8080/auth/realms/kong/.well-known/openid-configuration"
+export CLIENT_SECRET="$(jq '.clients[] | select(.clientId == "kong")' /home/ubuntu/edu-kgac-202/exercises/oidc/kong_realm_template.json | jq .secret | xargs)"
+export KEYCLOAK_URL="http://$STRIGO_RESOURCE_DNS:8080"
